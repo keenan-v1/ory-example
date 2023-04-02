5b56d7cc85dcf22a56aa643cd44e6a9afdfd486b
Keenan <jonathan@wantsmore.coffee>
2023-04-02 17:50:08 -0400

    Enable DNS support for the VPC

--- infra/network/main.tf
+++ infra/network/main.tf
@@ -10,6 +10,9 @@ module "vpc" {
   public_subnets   = var.public_subnet_cidrs
   database_subnets = var.database_subnet_cidrs
 
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
   database_subnet_group_name = "${var.project_name}-${var.environment}-database-subnet-group"
 
   enable_ipv6 = true
