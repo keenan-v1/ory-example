29e63dd1111935d3769d94fe53b24da0a86085d0
Keenan <jonathan@wantsmore.coffee>
2023-04-05 01:06:22 -0400

    Do you believe in life after love?

--- infra/ecr/main.tf
+++ infra/ecr/main.tf
@@ -13,7 +13,7 @@ locals {
 # Create an ECR repositories
 resource "aws_ecr_repository" "repositories" {
   for_each             = local.repositories
-  name                 = "/${var.organization}/${var.project_name}/${var.environment}/${each.value}"
+  name                 = "${var.organization}/${var.project_name}/${var.environment}/${each.value}"
   image_tag_mutability = "MUTABLE"
   force_delete         = true
 }
