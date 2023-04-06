8c6511e68fc13580fd22f9a21f6c8ba9f1d4c140
Keenan <jonathan@wantsmore.coffee>
2023-04-06 09:36:23 -0400

    Fixes

--- infra/database/main.tf
+++ infra/database/main.tf
@@ -127,6 +127,7 @@ data "aws_iam_policy_document" "ecs_execution_assume_role_policy" {
     principals {
       type = "Service"
       identifiers = [
+        "ecs-tasks.amazonaws.com",
         "ecs.amazonaws.com"
       ]
     }
