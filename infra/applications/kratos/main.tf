8c12a9e853a7f8231e1b29f6c2be18a72a967106
Keenan <jonathan@wantsmore.coffee>
2023-04-06 21:27:38 -0400

    Fixes...

--- infra/applications/kratos/main.tf
+++ infra/applications/kratos/main.tf
@@ -99,7 +99,7 @@ module "alb" {
     {
       name_prefix          = "pub-"
       backend_protocol     = "HTTP"
-      backend_port         = 0
+      backend_port         = 4433
       target_type          = "instance"
       deregistration_delay = 10
       health_check = {
@@ -117,7 +117,7 @@ module "alb" {
     {
       name_prefix          = "adm-"
       backend_protocol     = "HTTP"
-      backend_port         = 0
+      backend_port         = 4434
       target_type          = "instance"
       deregistration_delay = 10
       health_check = {
